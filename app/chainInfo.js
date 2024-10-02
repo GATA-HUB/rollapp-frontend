faccbe98bc680f455016530a3dd45035666bf892
ranchgainers <ranch_gainers0b@icloud.com>
2024-10-02 04:00:34 +0200

    add devnet playground

--- app/chainInfo.js
+++ app/chainInfo.js
@@ -1,5 +1,5 @@
 "use client";
-export let REACT_APP_NETWORK_ID = 1234;
+export let REACT_APP_NETWORK_ID = 10781;
 if (typeof window !== 'undefined') {
   REACT_APP_NETWORK_ID = window.localStorage.getItem('selectedNetworkID') || REACT_APP_NETWORK_ID;
 }
@@ -19,7 +19,7 @@ export const chainInfo = {
     REACT_APP_NODES: ["https://zksync2-testnet.zksync.dev", "https://testnet.era.zksync.dev"],
     REACT_APP_SUBGRAPH:
       "https://api.thegraph.com/subgraphs/name/kroim/erc721-zksync-testnet",
-    REACT_APP_BASE_URL: "https://weave.tyche.zone",
+    REACT_APP_BASE_URL: "https://gatahub.zone/",
   },
   324: {
     REACT_APP_NETWORK_ID: 324,
@@ -32,7 +32,7 @@ export const chainInfo = {
     REACT_APP_NODES: ["https://mainnet.era.zksync.io","https://zksync-era.blockpi.network/v1/rpc/public"],
     REACT_APP_SUBGRAPH:
       "https://api.studio.thegraph.com/query/50580/tyche-stake-all-nfts/0.0.1",
-    REACT_APP_BASE_URL: "https://weave.tyche.zone",
+    REACT_APP_BASE_URL: "https://gatahub.zone/",
   },
   84531: {
     REACT_APP_NETWORK_ID: 84531,
@@ -45,7 +45,7 @@ export const chainInfo = {
     REACT_APP_NODES: ["https://base-goerli.public.blastapi.io", "https://1rpc.io/base-goerli"],
     REACT_APP_SUBGRAPH:
       "https://api.studio.thegraph.com/query/50580/base-nfts/version/latest",
-    REACT_APP_BASE_URL: "https://weave.tyche.zone",
+    REACT_APP_BASE_URL: "https://gatahub.zone/",
   },
   8453: {
     REACT_APP_NETWORK_ID: 8453,
@@ -58,7 +58,7 @@ export const chainInfo = {
     REACT_APP_NODES: ["https://base-mainnet.public.blastapi.io", "https://base.blockpi.network/v1/rpc/public"],
     REACT_APP_SUBGRAPH:
       "https://api.studio.thegraph.com/query/50580/base-selected-contracts-only/1.0.0",
-    REACT_APP_BASE_URL: "https://weave.tyche.zone",
+    REACT_APP_BASE_URL: "https://gatahub.zone/",
   },
   // 1234: { // local
   //   REACT_APP_NETWORK_ID: 1234,
@@ -70,7 +70,7 @@ export const chainInfo = {
   //   REACT_APP_NODES: ["http://0.0.0.0:8545"],
   //   REACT_APP_SUBGRAPH:
   //       "https://api.studio.thegraph.com/query/50580/base-selected-contracts-only/1.0.0",
-  //   REACT_APP_BASE_URL: "https://weave.tyche.zone",
+  //   REACT_APP_BASE_URL: "https://gatahub.zone/",
   // },
   1234: {
     REACT_APP_NETWORK_ID: 1234,
@@ -82,7 +82,7 @@ export const chainInfo = {
     REACT_APP_NODES: ["https://rpc-dym-roller-testnet.gatahub.zone"],
     REACT_APP_SUBGRAPH:
       "https://api.studio.thegraph.com/query/50580/base-selected-contracts-only/1.0.0",
-    REACT_APP_BASE_URL: "https://weave.tyche.zone",
+    REACT_APP_BASE_URL: "https://gatahub.zone/",
   },
   100004: {
     REACT_APP_NETWORK_ID: 100004,
@@ -94,6 +94,18 @@ export const chainInfo = {
     REACT_APP_NODES: ["https://json-rpc.rolxtwo.evm.ra.blumbus.noisnemyd.xyz"],
     REACT_APP_SUBGRAPH:
       "https://api.studio.thegraph.com/query/50580/base-selected-contracts-only/1.0.0",
-    REACT_APP_BASE_URL: "https://weave.tyche.zone",
+    REACT_APP_BASE_URL: "https://gatahub.zone/",
+  },
+  10781: {
+    REACT_APP_NETWORK_ID: 10781,
+    REACT_APP_BLOCK_EXPLORER: "https://pg.dym.fyi/",
+    REACT_APP_NETWORK_ICON: "https://goerli.basescan.org/images/svg/brands/main.svg?v=23.08.01.1",
+    REACT_APP_NETWORK: "Gata",
+    REACT_APP_COIN: "agata",
+    REACT_APP_NODE_1: "https://evm.young-elephant-31.telebit.io",
+    REACT_APP_NODES: ["https://evm.young-elephant-31.telebit.io"],
+    REACT_APP_SUBGRAPH:
+      "https://api.studio.thegraph.com/query/50580/base-selected-contracts-only/1.0.0",
+    REACT_APP_BASE_URL: "https://gatahub.zone/",
   },
 };
